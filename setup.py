2118571b6038fc3727e967e81f248ec843edcfb1
Mohamad Mansouri <mmrota@X1Carbon>
2022-02-07 16:09:44 +0100

    Threshold JL implementation
    uodated our protocol
    changed project structure

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
+import email
 from setuptools import setup, find_packages
 
-setup(name='ftsa', version='1.0', packages=find_packages())
+
+setup(name='ftsa',
+ version='1.0',
+ author='Mohamad Mansouri',
+ email='mohamad.mansouri@thalesgroup.com',
+ packages=find_packages())
