eb75e917ac5af8d8801bac271b7cd18ed9915818
Mohamad Mansouri <mmrota@X1Carbon>
2022-09-14 16:56:01 +0200

    Fix setup

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
-import email
 from setuptools import setup, find_packages
 
 
 setup(name='ftsa',
  version='1.0',
  author='Mohamad Mansouri',
- email='mohamad.mansouri@thalesgroup.com',
+ author_email='mohamad.mansouri@thalesgroup.com',
  packages=find_packages(),
  python_requires=">=3.6",
  install_requires=[
